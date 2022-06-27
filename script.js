be580a49f9bbd4837451349d201362a30d5c593c
Ozi <oguzkaanyildiz@Ozis-MacBook-Air.local>
2022-06-27 20:03:31 +0200

    Making the previous-next functionality

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+    {
+        name: 'misc1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Mugu'
+    },
+    {
+        name: 'misc2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Mugu'
+    },
+    {
+        name: 'misc3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Mugu'
+    },
+    {
+        name: 'misc4',
+        displayName: 'Front Row (Remix)',
+        artist: 'Mugu'
+    }
+]
 
 // Check if playing
 let isPlaying = false;
@@ -24,3 +51,42 @@ function pauseSong() {
 
 // Play-Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+// Previous Song
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Next Song
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// On Load - Select First Song
+loadSong(songs[0]);
+
+// Prev-Next Event Listener
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
